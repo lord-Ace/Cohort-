14a59aeb6d10ebfdc45b0af70879dad10be44c93
lord-Ace <emmanuelashinze612@gmail.com>
2025-06-04 21:32:15 +0100

    ran npm fund

--- task6/src/App.jsx
+++ task6/src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+const food = <header>uogfvbvbb</header>
 function App() {
  
   return (
     <>
       <div>
         <h1>HELLO WORLD</h1>
+        {food}
         </div>
     </>
   )
